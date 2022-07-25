fe9057d7727f8bed30d0e3606e5a764dc701b4b2
PabloGomez10 <102038179+PabloGomez10@users.noreply.github.com>
2022-07-25 18:01:48 -0300

    a
    Desafio

---
+++ js/miarchivo.js
@@ -0,0 +1,9 @@
+debugger;
+
+let Bienvenido = prompt ("ingresar tu nombre");
+
+while(Bienvenido != "Pablo"){
+
+alert ("Ingresa tu nombre "+ Bienvenido) ;
+Bienvenido = prompt("ingresar otro nombre") ;
+}
